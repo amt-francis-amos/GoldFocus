7deb7a33ce5bb3de37b43431a97ffc4dc717b241
amt-francis-amos <sugartone45@yahoo.com>
2025-02-13 06:29:17 -0800

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
+import About from "./pages/About";
+import Services from "./pages/services";
 
 const App = () => {
   const isAuthenticated = localStorage.getItem("isAuthenticated") === "true";
@@ -19,6 +21,12 @@ const App = () => {
         <Route path="/" element={isAuthenticated ? <Navigate to="/home" /> : <Navigate to="/login" />} />
         
         <Route path="/login" element={<Login />} />
+        
+        <Route path="/about" element={<About />} />
+        
+        <Route path="/services" element={<Services />} />
+        
+        <Route path="/contact" element={<Login />} />
         
         
         <Route path="/home" element={isAuthenticated ? <Home /> : <Navigate to="/login" />} />
