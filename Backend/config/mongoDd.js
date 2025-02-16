966bceedc7bf36924ae5d80f6fc3c42a234b6c15
amt-francis-amos <sugartone45@yahoo.com>
2025-02-15 18:54:26 -0800

    updated

---
+++ Backend/config/mongoDd.js
@@ -0,0 +1,18 @@
+import mongoose  from  'mongoose'
+
+const mongoDb = async () => {
+  
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      dbName: "Fintech-Db", 
+    });
+
+    console.log("Database Connected");
+  } catch (error) {
+    console.error("Error connecting to the database:", error.message);
+    process.exit(1); 
+  }
+};
+
+
+export default mongoDb;
---
+++ Backend/server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config'; 
+import connectDB from './config/mongoDd.js'; 
+import userRoutes from './routes/userRoutes.js'; 
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors()); 
+
+
+app.use('/api/users', userRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
