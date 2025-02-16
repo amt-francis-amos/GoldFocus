b94924b3d1edbd5b3d7c089113518e1680807943
amt-francis-amos <sugartone45@yahoo.com>
2025-02-16 02:30:33 -0800

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import Footer from "./components/Footer";
@@ -9,17 +9,20 @@ import Services from "./pages/Services";
 import Home from "./pages/Home";
 
 const App = () => {
+  const location = useLocation();
+
   return (
     <div>
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
-       <Route path="/" element={<Home />}/>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <Footer />
+      {/* Hide Footer on the Login page */}
+      {location.pathname !== "/login" && <Footer />}
     </div>
   );
 };
