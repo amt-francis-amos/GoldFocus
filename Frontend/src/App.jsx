49ea2913bf43038686e02afcd1dc9fe36dd08ae5
amt-francis-amos <sugartone45@yahoo.com>
2025-02-15 19:49:53 -0800

    updated

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
 
       <Routes>
         <Route path="/login" element={<Login />} />
-       <Route path="/home" element={<Home />}/>
+       <Route path="/" element={<Home />}/>
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
